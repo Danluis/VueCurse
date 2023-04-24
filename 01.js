722a3d4552b955ea3f096debd639a9c5049c8b31
Danluis <Danluis2a25@gmail.com>
2023-04-24 12:49:53 -0400

    v-Bind

--- 01.js
+++ 01.js
@@ -5,13 +5,8 @@ createApp({
    
     return {
       titulo: 'Vue es un framework de javascript',
-      frutas: [
-          {nombre:'Pera', cantidad:10},
-          {nombre:'Manzana', cantidad:0},
-          {nombre:'Platano', cantidad:11}
-      ],
-      nuevaFruta:'',
-      total:0
+      fondo: 'bg-warning',
+      color: false
     }
   },
     methods: {
@@ -22,16 +17,6 @@ createApp({
         this.nuevaFruta = '';
         
       }
-    },
-    computed: {
-      sumarFrutas(){
-        this.total = 0;
-        for(fruta of this.frutas){
-          this.total = this.total + fruta.cantidad;
-        }
-        return this.total;
-      }
-    }
-   
+    }   
   
 }).mount('#app')
