d75e4b12f540013ec5f9328229f1c50ae34562fc
Danluis <Danluis2a25@gmail.com>
2023-04-24 13:58:48 -0400

    CRUD LocalStorage

--- 01.js
+++ 01.js
@@ -4,19 +4,38 @@ createApp({
   data() {
    
     return {
-      titulo: 'Vue es un framework de javascript',
-      fondo: 'bg-warning',
-      color: false
+      titulo: 'GYM con Vue',
+      tareas: [],
+      nuevaTarea: ''
     }
   },
     methods: {
-      agregarFruta() {
-        this.frutas.push({
-          nombre: this.nuevaFruta, cantidad:0
-        });
-        this.nuevaFruta = '';
-        
+      agregarTarea: function(){
+       // console.log('diste Click', this.nuevaTarea);
+       this.tareas.push({
+        nombre: this.nuevaTarea,
+        estado:false
+       })
+        this.nuevaTarea='';
+        localStorage.setItem('gym-vue',JSON.stringify(this.tareas));
+      },
+      editarTarea: function(index){
+        this.tareas[index].estado = true
+        localStorage.setItem('gym-vue',JSON.stringify(this.tareas));
+      },
+      eliminar: function(index){
+        this.tareas.splice(index, 1);
+        localStorage.setItem('gym-vue',JSON.stringify(this.tareas));
       }
-    }   
+    },
+    created: function(){
+      let datosDB = JSON.parse(localStorage.getItem('gym-vue'));
+      if(datosDB === null){
+        this.tareas = [];
+      }else{
+        this.tareas = datosDB;
+      }
+    }
+   
   
 }).mount('#app')
